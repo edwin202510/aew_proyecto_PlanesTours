fe554428a32251000092d2e638ad87ffdba92170
edwin202510 <i202419274@cibertec.edu.pe>
2025-10-28 04:09:20 +0000

    cambio en boton buscar formulario

--- Pagina-Destinos/JS-Destinos/codigo.js
+++ Pagina-Destinos/JS-Destinos/codigo.js
@@ -1,19 +1,19 @@
-alert ("Hola Mundo");
+alert("Hola Mundo");
 
-var dato="Juan";
+var dato = "Juan";
 alert("Hola" + dato);
 
-let nombre=prompt("Ingrese su nombre");
-alert("Hola"+ nombre);
+let nombre = prompt("Ingrese su nombre");
+alert("Hola" + nombre);
 
-const formulario=document.getElementById('form-registro');
-const resultado=document.getElementById('resultado');
+const formulario = document.getElementById('form-registro');
+const resultado = document.getElementById('resultado');
 document.getElementById('btn-buscar').addEventListener('click', () => {
-    const ticket=document.getElementById('numvuelo').value.trim();
-    const ruta=document.getElementById('ruta').value.trim();
-    if (!ruta || !ticket){
-        resultado.textContent='selecciona ruta o ticket';
-        resultado.style.color='red';
+    const ticket = document.getElementById('numvuelo').value.checked();
+    const ruta = document.getElementById('ruta').value.checked();
+    if (!ruta || !ticket) {
+        resultado.textContent = 'selecciona ruta o ticket';
+        resultado.style.color = 'red';
         return;
-    }   
+    }
 });
