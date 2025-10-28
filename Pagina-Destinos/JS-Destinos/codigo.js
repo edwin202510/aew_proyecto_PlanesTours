0c3f3c5fe11b1b7f944cbe5311a958a11f6a398a
edwin202510 <i202419274@cibertec.edu.pe>
2025-10-28 03:35:37 +0000

    editando script

--- Pagina-Destinos/JS-Destinos/codigo.js
+++ Pagina-Destinos/JS-Destinos/codigo.js
@@ -1,8 +1,19 @@
-/*alert ("Hola Mundo");*/
+alert ("Hola Mundo");
 
-/*var dato="Juan";
-alert("Hola" + dato);*/
+var dato="Juan";
+alert("Hola" + dato);
 
-/*let nombre=prompt("Ingrese su nombre");
-alert("Hola"+ nombre);*/
+let nombre=prompt("Ingrese su nombre");
+alert("Hola"+ nombre);
 
+const formulario=document.getElementById('form-registro');
+const resultado=document.getElementById('resultado');
+document.getElementById('btn-buscar').addEventListener('click', () => {
+    const ticket=document.getElementById('numvuelo').value.trim();
+    const ruta=document.getElementById('ruta').value.trim();
+    if (!ruta || !ticket){
+        resultado.textContent='selecciona ruta o ticket';
+        resultado.style.color='red';
+        return;
+    }   
+});
